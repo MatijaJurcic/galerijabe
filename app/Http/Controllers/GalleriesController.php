c9121e8dd44413778ca552165dd79eee71e2c260
MatijaJurcic <matijajurcic2207@gmail.com>
2023-02-02 18:36:23 +0100

    Controllers and Models

---
+++ app/Http/Controllers/GalleriesController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\GalleryRequest;
+use App\Models\Gallery;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class GalleryController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => 'index']);
+    }
+
+    public function index()
+    {
+        $galleries =  Gallery::with('user')->orderBy('id', 'desc')->paginate(10);
+        if ($galleries->isEmpty()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Ooops no galleries',
+            ], 404);
+        };
+        return $galleries;
+    }
+
+    public function show($id)
+    {
+        return Gallery::with('user')->with('comments')->findOrFail($id);
+    }
+
+    public function store(GalleryRequest $request)
+    {
+        $gallery = $request->all();
+        $gallery['user_id'] = Auth::user()->id;
+        return Gallery::create($gallery);
+    }
+
+    public function update($id, GalleryRequest $request)
+    {
+        return Gallery::findOrFail($id)->update($request->all());
+    }
+
+    public function destroy($id)
+    {
+        return Gallery::destroy($id);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,15 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -18,7 +18,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
     ];
@@ -41,4 +42,34 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    public function galleries()
+    {
+        return $this->hasMany(Gallery::class)->orderBy('id');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
